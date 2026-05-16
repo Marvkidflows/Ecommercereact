0087bae85181a4577a0e1bddb87c8f01aaaebacf
Your Name <you@example.com>
2026-05-16 08:41:41 +0100

    fix import casing

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,7 @@ import Accessories from "./Pages/Accessories";
 import AboutUsPage from "./Pages/AboutUsPage";
 import ContactUs from "./Pages/ContactUs";
 import DescriptionProduct from "./Components/DescriptionProduct";
-import LoginPage from "./Pages/LoginPage";
-import Register from "./Pages/registerPage";
+
 const App = () => {
   return (
     <CartProvider>   {/* ✅ wrap all routes */}
@@ -24,8 +23,7 @@ const App = () => {
         <Route path="/aboutus" element={<AboutUsPage />} />
         <Route path="/contact-us" element={<ContactUs />} />
       <Route path='/description/:id' element={<DescriptionProduct />} />
-      {/* < Route path="/" element = {<LoginPage/>}/>
-      <Route path="register" element={<Register/>}/> */}
+     
       </Routes>
     </CartProvider>
   );
